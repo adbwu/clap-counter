de849ea8325c8e140302830ed46c191f3f8b55c9
Allison Budziak Wu <112855952+adbwu@users.noreply.github.com>
2023-09-04 13:49:40 -0400

    function to access user audio input

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [audioStream, setAudioStream] = useState(null);
+
+  //Function to request audio from user on button click
+  const startRecording = async () => {
+    try {
+      const userAudio = await navigator.mediaDevices.getUserMedia({ audio: true });
+      setAudioStream(userAudio);
+    } catch (error) {
+      console.error("Error accessing user media:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Clap Counter</h1>
       </header>
       <main>
-        <button>Mic On</button>
+        <button onClick={startRecording}>Mic On</button>
       </main>
     </div>
   );
